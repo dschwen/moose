73fc6d376b8d4544b183805f4b7f965c7e3ccd15
Daniel Schwen <daniel.schwen@inl.gov>
2014-11-06 17:32:36 -0700

    Extend and fix R4T unit test

--- unit/src/RankFourTensorTest.C
+++ unit/src/RankFourTensorTest.C
@@ -40,17 +40,42 @@ RankFourTensorTest::~RankFourTensorTest()
 {}
 
 void
+RankFourTensorTest::SubtractTest()
+{
+  RankFourTensor i = _m1;
+  i -= _m0;
+  i -= _m0;
+
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(0.0, i.L2norm(), 0.0001);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(0.0, (_m2 - _m1 -_m0).L2norm(), 0.0001);
+}
+
+void
+RankFourTensorTest::AddTest()
+{
+  RankFourTensor i = _m0;
+  i += _m1;
+  i += _m2;
+
+  const Real l2 = 74.2240791118355;
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(l2 * 6.0, i.L2norm(), 0.0001);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(l2 * 6.0, (_m2 + _m1 +_m0).L2norm(), 0.0001);
+}
+
+void
 RankFourTensorTest::L2normTest()
 {
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(0, _m0.L2norm(), 0.0001);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(1.732051, _m1.L2norm(), 0.0001);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(3.741657, _m2.L2norm(), 0.0001);
+  const Real l2 = 74.2240791118355;
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(l2, _m0.L2norm(), 0.0001);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(2.0 * l2, _m1.L2norm(), 0.0001);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(3.0 * l2, _m2.L2norm(), 0.0001);
 }
 
 void
 RankFourTensorTest::transposeTest()
 {
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(0, (_m0.transposeMajor() - _m0).L2norm(), 0.0001);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(0, (_m1.transposeMajor() - _m1).L2norm(), 0.0001);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(0, (_m2.transposeMajor() - _m2).L2norm(), 0.0001);
+  const Real l2 = 46.7974153132415;
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(l2, (_m0.transposeMajor() - _m0).L2norm(), 0.0001);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(2.0 * l2, (_m1.transposeMajor() - _m1).L2norm(), 0.0001);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(3.0 * l2, (_m2.transposeMajor() - _m2).L2norm(), 0.0001);
 }
