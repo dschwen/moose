cf6d59b6996216e605be22c97fecce55cb66051a
Daniel Schwen <daniel.schwen@inl.gov>
2016-11-30 17:12:34 -0700

    Start work (#8058)

--- modules/phase_field/include/postprocessors/GrainDataTracker.h
+++ modules/phase_field/include/postprocessors/GrainDataTracker.h
@@ -9,53 +9,87 @@
 #define GRAINDATATRACKER_H
 
 #include "GrainTracker.h"
+#include "GrainDataUserObject.h"
+
+/// base class for storage purposes
+class GrainDataBase {};
+
+/// data vector template calss
+template<class T>
+class GrainDataM<T> : public GrainDataBase
+{
+public:
+  const T & getData(unsigned int grain_id) const;
+
+protected:
+  std::vector<T> _grain_data;
+}
 
 /**
- * GrainTracker derived class template to base objects on which maintain physical
+ * GrainDataTracker is a GrainTracker object which maintains which maintains physical
  * parameters for individual grains.
  */
-template <typename T>
 class GrainDataTracker : public GrainTracker
 {
 public:
   GrainDataTracker(const InputParameters & parameters);
 
   /// return data for selected grain
-  const T & getData(unsigned int grain_id) const;
+  template<typename T>
+  const GrainData<T> & getColumn(const std::string & column) const;
 
 protected:
-  /// implement this method to initialize the data for the new grain
-  virtual T newGrain(unsigned int new_grain_id) = 0;
+  virtual void newGrainCreated(unsigned int new_grain_id) override;
 
-  virtual void newGrainCreated(unsigned int new_grain_id);
-
+private:
   /// per grain data
-  std::vector<T> _grain_data;
+  std::map<std::string, std::pair<GrainDataUserObject *, GrainDataBase *>> _grain_data_columns;
 };
 
 
-template <typename T>
-GrainDataTracker<T>::GrainDataTracker(const InputParameters & parameters) :
+GrainDataTracker::GrainDataTracker(const InputParameters & parameters) :
     GrainTracker(parameters)
 {
 }
 
 template <typename T>
+const GrainData<T> &
+GrainDataTracker::getColumn<T>(const std::string & column) const
+{
+  auto colit = _grain_data_columns.find(column);
+  if (colit != _grain_data_columns.end())
+  {
+    auto datptr = dynamic_cast<T *>(colit->second.second);
+    if (dataptr != nullptr)
+      return *dataptr
+    else
+      mooseError("The requested data column has the wrong type.");
+  }
+  else
+    mooseError("Data column '" << column << "' not available.");
+}
+
+template <typename T>
 const T &
-GrainDataTracker<T>::getData(unsigned int grain_id) const
+GrainDataM<T>::getData(unsigned int grain_id) const
 {
   mooseAssert(grain_id < _grain_data.size(), "Requested data for invalid grain index.");
   return _grain_data[grain_id];
 }
 
-template <typename T>
 void
-GrainDataTracker<T>::newGrainCreated(unsigned int new_grain_id)
+GrainDataTracker::newGrainCreated(unsigned int new_grain_id)
 {
-  if (_grain_data.size() <= new_grain_id)
-    _grain_data.resize(new_grain_id + 1);
+  for (auto & col : _grain_data_columns)
+    col.second.first->newGrainCreated(unsigned int new_grain_id, col.second.second);
+}
 
-  _grain_data[new_grain_id] = newGrain(new_grain_id);
+
+template<class T>
+const T &
+GrainDataBase::getData<T>(unsigned int grain_id) const
+{
+  return T();
 }
 
 #endif // GRAINDATATRACKER_H
