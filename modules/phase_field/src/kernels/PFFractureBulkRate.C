8b1a6d7408a715916120aaccddd792d57766369d
Daniel Schwen <daniel.schwen@inl.gov>
2017-06-16 14:59:14 -0600

    Add non-split version (#9329)

--- modules/phase_field/src/kernels/PFFractureBulkRate.C
+++ modules/phase_field/src/kernels/PFFractureBulkRate.C
@@ -4,62 +4,53 @@
 /*          All contents are licensed under LGPL V2.1           */
 /*             See LICENSE for full restrictions                */
 /****************************************************************/
-#include "SplitPFFractureBulkRate.h"
+#include "PFFractureBulkRate.h"
+#include "MooseVariable.h"
 #include "RankTwoTensor.h"
 #include "MathUtils.h"
 
 template <>
 InputParameters
-validParams<SplitPFFractureBulkRate>()
+validParams<PFFractureBulkRate>()
 {
   InputParameters params = validParams<PFFractureBulkRateBase>();
   params.addClassDescription(
       "Kernel to compute bulk energy contribution to damage order parameter residual equation");
-  params.addRequiredCoupledVar("beta", "Laplacian of the kernel variable");
   return params;
 }
 
-SplitPFFractureBulkRate::SplitPFFractureBulkRate(const InputParameters & parameters)
-  : PFFractureBulkRateBase(parameters), _beta(coupledValue("beta")), _beta_var(coupled("beta"))
+PFFractureBulkRate::PFFractureBulkRate(const InputParameters & parameters)
+  : PFFractureBulkRateBase(parameters), _second_u(second()), _second_phi(secondPhi())
 {
 }
 
 Real
-SplitPFFractureBulkRate::precomputeQpResidual()
+PFFractureBulkRate::precomputeQpResidual()
 {
   const Real & gc = _gc_prop[_qp];
   const Real & c = _u[_qp];
-  const Real x = _width * _beta[_qp] + 2.0 * (1.0 - c) * _G0_pos[_qp] / gc - c / _width;
+  const Real beta = _second_u[_qp].tr();
+  const Real x = _width * beta + 2.0 * (1.0 - c) * _G0_pos[_qp] / gc - c / _width;
 
   return -MathUtils::positivePart(x) / _viscosity;
 }
 
 Real
-SplitPFFractureBulkRate::precomputeQpJacobian()
+PFFractureBulkRate::precomputeQpJacobian()
 {
   const Real & gc = _gc_prop[_qp];
   const Real & c = _u[_qp];
-  const Real x = _width * _beta[_qp] + 2.0 * (1.0 - c) * _G0_pos[_qp] / gc - c / _width;
-  const Real dx = -2.0 * _G0_pos[_qp] / gc - 1.0 / _width;
+  const Real beta = _second_u[_qp].tr();
+  const Real x = _width * beta + 2.0 * (1.0 - c) * _G0_pos[_qp] / gc - c / _width;
+  const Real dx = _width * _second_phi[_j][_qp].tr() - 2.0 * _phi[_j][_qp] * _G0_pos[_qp] / gc -
+                  _phi[_j][_qp] / _width;
 
-  return -MathUtils::heavyside(x) / _viscosity * dx * _phi[_j][_qp];
+  return -MathUtils::heavyside(x) / _viscosity * dx;
 }
 
 Real
-SplitPFFractureBulkRate::computeQpOffDiagJacobian(unsigned int jvar)
+PFFractureBulkRate::computeQpOffDiagJacobian(unsigned int jvar)
 {
-
-  const Real & c = _u[_qp];
-  const Real & gc = _gc_prop[_qp];
-  const Real x = _width * _beta[_qp] + 2.0 * (1.0 - c) * (_G0_pos[_qp] / gc) - c / _width;
-
-  // Contribution of Laplacian split variable
-  if (jvar == _beta_var)
-  {
-    const Real xfacbeta = -MathUtils::heavyside(x) / _viscosity * _width;
-    return xfacbeta * _phi[_j][_qp] * _test[_i][_qp];
-  }
-
   // bail out early if no stress derivative has been provided
   if (_dG0_pos_dstrain == NULL)
     return 0.0;
@@ -73,7 +64,13 @@ SplitPFFractureBulkRate::computeQpOffDiagJacobian(unsigned int jvar)
   // Contribution of displacements to off-diagonal Jacobian of c
   if (c_comp < _ndisp)
   {
+    const Real & c = _u[_qp];
+    const Real & gc = _gc_prop[_qp];
+    const Real beta = _second_u[_qp].tr();
+
+    const Real x = _width * beta + 2.0 * (1.0 - c) * _G0_pos[_qp] / gc - c / _width;
     const Real xfac = -MathUtils::heavyside(x) / _viscosity * 2.0 * (1.0 - c) / gc;
+
     Real val = 0.0;
     for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
       val += ((*_dG0_pos_dstrain)[_qp](c_comp, i) + (*_dG0_pos_dstrain)[_qp](i, c_comp)) / 2.0 *
