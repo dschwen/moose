6648b53e1e798184186ce01ebfa8e8aa94a02f42
Daniel Schwen <daniel.schwen@inl.gov>
2025-08-27 12:57:06 -0600

    scaffold sqa and unit tests

--- modules/shallow_water/src/base/ShallowWaterApp.C
+++ modules/shallow_water/src/base/ShallowWaterApp.C
@@ -19,18 +19,16 @@ InputParameters
 ShallowWaterApp::validParams()
 {
   InputParameters params = MooseApp::validParams();
-  params.set<bool>("automatic_automatic_scaling") = false;
   params.set<bool>("use_legacy_material_output") = false;
-  params.set<bool>("use_legacy_initial_residual_evaluation_behavior") = false;
   return params;
 }
-
-registerKnownLabel("ShallowWaterApp");
 
 ShallowWaterApp::ShallowWaterApp(const InputParameters & parameters) : MooseApp(parameters)
 {
   ShallowWaterApp::registerAll(_factory, _action_factory, _syntax);
 }
+
+// registerKnownLabel("ShallowWaterApp");
 
 ShallowWaterApp::~ShallowWaterApp() {}
 
@@ -51,6 +49,9 @@ ShallowWaterApp::registerApps()
   registerApp(ShallowWaterApp);
 }
 
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
 extern "C" void
 ShallowWaterApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
 {
--- modules/shallow_water/src/main.C
+++ modules/shallow_water/src/main.C
@@ -7,12 +7,12 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "ShallowWaterApp.h"
+#include "ShallowWaterTestApp.h"
 #include "MooseMain.h"
 
 // Begin the main program.
 int
 main(int argc, char * argv[])
 {
-  return Moose::main<ShallowWaterApp>(argc, argv);
+  return Moose::main<ShallowWaterTestApp>(argc, argv);
 }
